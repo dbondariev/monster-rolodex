1ee429842bbccdfe9bc2aa434ac2557407206d4b
Dima Bondariev <dima.bondariev@gmail.com>
2022-12-19 23:06:57 +0200

    added state as a constructor

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import { Component } from 'react';
 
 import logo from './logo.svg';
 import './App.css';
+import { name } from 'tar/lib/types';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      name: 'User'
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi {this.state.name}, I am a React App
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button>
+            Change Name
+          </button>
         </header>
       </div>
     );
