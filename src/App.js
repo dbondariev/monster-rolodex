7a684d800939c18ce621929fe25a1a0062d33cdd
Dima Bondariev <dima.bondariev@gmail.com>
2022-12-20 16:34:02 +0200

    addeded search functionality with filter and state

--- src/App.js
+++ src/App.js
@@ -6,22 +6,50 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users }))
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users};
+          },
+          () => {
+            console.log(this.state.monsters);
+          }
+        )
+      );
   }
+
+
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          className='search-box'
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
-              <h1>{monster.name}</h1>
+              <h1 key={monster.id}>{monster.name}</h1>
             </div>
           )
         })}
@@ -29,4 +57,5 @@ class App extends Component {
     );
   }
 }
+
 export default App;
