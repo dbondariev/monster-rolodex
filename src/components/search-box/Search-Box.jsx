92d7177d5a8e5e8d159b4cda9afeeb8b70af81c7
Dima Bondariev <dima.bondariev@gmail.com>
2022-12-27 22:46:25 +0200

    finished trhe project fixed conflicts

--- src/components/search-box/Search-Box.jsx
+++ src/components/search-box/Search-Box.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Component } from 'react';
 import './Search-Box.css';
 
 const SearchBox = ({ className, placeholder, onChangeHandler }) => {
-	<input
-		className={`search-box ${className}`}
-		type='search'
-		placeholder={placeholder}
-		onChange={onChangeHandler}
-	/>;
+	return (
+		<input
+			className={`search-box ${className}`}
+			type='search'
+			placeholder={placeholder}
+			onChange={onChangeHandler}
+		/>
+	);
 };
 
 export default SearchBox;
